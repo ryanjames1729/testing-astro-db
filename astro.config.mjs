f219dcf86d63b436b4f800fe8dd0fdd77c2a2e5b
Ryan James <ryanjames1729@gmail.com>
2024-03-21 08:53:44 -0400

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [db(), tailwind(), react()],
+  adapter: vercel()
 });
