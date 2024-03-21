a39a2fc0645d76d722666239f139738a36fccd24
Ryan James <ryanjames1729@gmail.com>
2024-03-21 08:54:56 -0400

    wrong adapter, trying again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [db(), tailwind(), react()],
-  adapter: vercel()
+  adapter: netlify()
 });
